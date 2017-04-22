5c5ffae982442fa1300182468681608f20e5c723
Jami Schwarzwalder <jamischwarzwalder@gmail.com>
2017-04-22 14:23:37 -0700

    Created Project.  Started work on Inhabitant script

---
+++ Assets/Inhabitant.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inhabitant : MonoBehaviour
+{
+    private float radiusSquared;
+    public Transform ModelTransform;
+    public float Gravity;
+
+    // Use this for initialization
+    public void Start()
+    {
+        radiusSquared = ModelTransform.localPosition.sqrMagnitude;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (ModelTransform.localPosition.sqrMagnitude > radiusSquared)
+        {
+            Rigidbody modelRigidbody = ModelTransform.GetComponent<Rigidbody>();
+            modelRigidbody.velocity -= ModelTransform.localPosition.normalized * Gravity * Time.deltaTime;
+        }
+
+        if (ModelTransform.localPosition.sqrMagnitude < radiusSquared)
+        {
+            ModelTransform.localPosition = ModelTransform.localPosition.normalized * Mathf.Sqrt(radiusSquared);
+        }
+    }
+}
